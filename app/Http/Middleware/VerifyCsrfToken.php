8293905c6904fdf8ee0c2c9b8546c21b7d0ca163
DanielLorenzoS <daniellorenzo47@aragon.unam.mx>
2023-06-07 21:23:53 -0600

    CRFS 2

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'http://localhost:8000/*', 'https://laravel-production-e6f0.up.railway.app/*'
+        '*'
     ];
+    
 }
